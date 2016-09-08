7e92a85478c40c5dfe7721d67902ff1ad5cbcaad
SarCasm <sarcasm008@gmail.com>
2016-09-09 00:44:52 +0300

    Singleton

--- app/crawler-loop.rb
+++ app/crawler-loop.rb
@@ -5,6 +5,6 @@ MIN_IN_HOUR     = 60
 SECONDS_IN_HOUR = SECONDS_IN_MIN * MIN_IN_HOUR
 
 loop do
-  Crawler.run
+  Crawler.instance.run
   sleep(SECONDS_IN_HOUR * 3)
 end
--- app/crawler.rb
+++ app/crawler.rb
@@ -1,49 +1,63 @@
 require 'mechanize'
 require 'sqlite3'
+requrie 'singleton'
+
 require 'awesome_print'
 require 'pry'
 
 
 class Crawler
+  include Singleton
   SOURCE_WEBSITE = "http://l2tops.ru/"
 
+  def initialize
+    @server_db = SQLite3::Database.new "servers.db"
+    @result_db = SQLite3::Database.new "results.db"
+    @agent = Mechanize.new
+  end
+
   def self.run
-    server_db = SQLite3::Database.new "servers.db"
-    result_db = SQLite3::Database.new "results.db"
-    agent = Mechanize.new
-
-    page = agent.get(SOURCE_WEBSITE)
-    page_servers = page.search('.server')
-
-    page_servers.each do |page_server|
+    connect
+    extract_nodes_from_page.each do |sever_node|
       begin
-        title       = page_server.at('.name').text
-        chronicles  = page_server.at('.chronicle').text
-        rates       = page_server.at('.rates').text
-        date        = page_server.at('.date').text
-
         server_db.execute(
           "insert into servers (title, chronicles, rates, date) values ( ?, ?, ?, ? )",
-          [title, chronicles, rates, date]
+          extract_data_from_node sever_node
         )
       rescue Exception => e
-        ap "Got error #{e} for page:"
-        ap page_server
+        ap "Got error #{e} for page:\n #{sever_node}"
       end
     end
+    save_results
+  end
 
-    servers = server_db.execute("select * from servers")
-    servers.each { |s| p s }
-    ap "Servers in total: #{servers.length}"
+  private
 
+  def self.connect
+    @page = @agent.get(SOURCE_WEBSITE)
+  end
+
+  def self.extract_nodes_from_page
+    @page.search('.server')
+  end
+
+  def self.extract_data_from_node(node)
+    title       = node.at('.name').text
+    chronicles  = node.at('.chronicle').text
+    rates       = node.at('.rates').text
+    date        = node.at('.date').text
+    [title, chronicles, rates, date]
+  end
+
+  def self.save_results
+    server_count = server_db.execute("select COUNT(*) from servers")
     begin
       result_db.execute(
         "insert into results (date, total_servers) values ( ?, ? )",
         [Time.now, servers.length]
        )
     rescue Exception => e
-      ap "Got error when saving results:"
-      ap e
+      ap "Got error when saving results: #{e}"
     end
   end
 end
