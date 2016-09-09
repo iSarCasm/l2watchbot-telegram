7e76b4e987863522ae1f5471e5d7f87a9ee915a2
SarCasm <sarcasm008@gmail.com>
2016-09-09 17:41:21 +0300

    changes

--- app/setup.rb
+++ app/setup.rb
@@ -1,6 +1,6 @@
 require 'pg'
 
-database = PG.connect( dbname: "l2watchbot", user: "postgres", password: "postgres" )
+database = PG.connect( dbname: "l2watchbot", user: "postgres", password: "postgres", hostaddr: "postgres://xmeveejkmcemfy:XxvCrubxgW5XbKHL8qvLjkPpFM@ec2-54-228-196-12.eu-west-1.compute.amazonaws.com:5432/d2ulj8p3qmn3v1" )
 
 database.exec <<-SQL
   create table servers (
