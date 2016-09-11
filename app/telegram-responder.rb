eb4b479838ea56e7f2a83e12c07b14a5eab75df1
SarCasm <sarcasm008@gmail.com>
2016-09-11 15:15:52 +0300

    Filter!!

--- app/telegram-responder.rb
+++ app/telegram-responder.rb
@@ -1,136 +1,283 @@
 require 'json'
 
 class TelegramResponder
-    def initialize(bot, database)
-      @bot = bot
-      @database = database
-    end
+  def initialize(bot, database)
+    @bot = bot
+    @database = database
+  end
 
-    def respond_to(message)
-      @all      = @database.exec("SELECT * from servers").values
-      @user     = User.new(@database, message.from.id)
-      case message
-      when Telegram::Bot::Types::CallbackQuery
-        respond_to_callback_query(message)
-      when Telegram::Bot::Types::Message
-        respond_to_text(message)
-      end
-    end
-
-    private
-
-    def respond_to_text(message)
-      case message.text
-      when '/start'
-        help(message)
-      when '/help'
-        help(message)
-      when '/soon'
-        send_servers soon(@all, 7), message
-        add_help_to_end(message)
-      when '/recent'
-        send_servers recent(@all, 7), message
-        add_help_to_end(message)
-      when '/info'
-        info(message)
-        add_help_to_end(message)
-      when '/filter'
-        kb = [
-          [Telegram::Bot::Types::KeyboardButton.new(text: 'C1-C4'),
-          Telegram::Bot::Types::KeyboardButton.new(text: 'Interlude')],
-          [Telegram::Bot::Types::KeyboardButton.new(text: 'Interlude+'),
-          Telegram::Bot::Types::KeyboardButton.new(text: 'High five')],
-          [Telegram::Bot::Types::KeyboardButton.new(text: 'Epilogue'),
-          Telegram::Bot::Types::KeyboardButton.new(text: 'Classic')],
-          Telegram::Bot::Types::KeyboardButton.new(text: 'Freya and newer'),
-          Telegram::Bot::Types::KeyboardButton.new(text: I18n.t('enough'))
-        ]
-        markup = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: kb, one_time_keyboard: true)
-        @bot.api.send_message(chat_id: message.chat.id, text: I18n.t('select_chronicle'), reply_markup: markup)
-      when '/notify'
-        @bot.api.send_message(chat_id: message.chat.id, text: "Not implemented yet!")
+  def filter_keyboard
+    types = ['C1-C4', 'Interlude', 'Epilogue', 'High five', 'Classic', I18n.t('other')]
+    types.delete('C1-C4') if @user.filter_chronicle.include?('c1')
+    types.delete('Interlude') if @user.filter_chronicle.include?('interlude')
+    types.delete('Epilogue') if @user.filter_chronicle.include?('epilogue')
+    types.delete('High five') if @user.filter_chronicle.include?('high five')
+    types.delete('Classic') if @user.filter_chronicle.include?('classic')
+    types.delete(I18n.t('other')) if @user.filter_chronicle.include?('freya')
+    pairs = []
+    types.each_with_index do |t, i|
+      if i.even? then
+        pairs << [t]
       else
-        @bot.api.send_message(chat_id: message.chat.id, text: message.text)
+        pairs.last << t
       end
     end
+    pairs << Telegram::Bot::Types::KeyboardButton.new(text: I18n.t('enough'))
+  end
 
-    def respond_to_callback_query(message)
-      case message.data
-      when 'C1-C4'
-        p 'rofl'
-      when 'Interlude'
-        p 'rofl'
-      when 'Interlude+'
-        p 'rofl'
-      when 'High five'
-        p 'rofl'
-      when 'Epilogue'
-        p 'rofl'
-      when 'Classic'
-        p 'rofl'
-      when 'Freya and newer'
-        p 'rofl'
+  def respond_to(message)
+    @all      = @database.exec("SELECT * from servers").values
+    @user     = User.new(@database, message.from.id)
+    I18n.locale = (@user.lang || :en).to_sym
+    case message
+    when Telegram::Bot::Types::CallbackQuery
+      respond_to_callback_query(message)
+    when Telegram::Bot::Types::Message
+      respond_to_text(message)
+    end
+  end
+
+  private
+
+  def respond_to_text(message)
+    case message.text
+    when '/start'
+      if @user.lang then
+        help(message)
+      else
+        ask_lang(message)
       end
+    when '/lang'
+      ask_lang(message)
+    when '/help'
+      help(message)
+    when '/soon'
+      send_servers soon(@all, 7), message
+      add_help_to_end(message)
+    when '/recent'
+      send_servers recent(@all, 7), message
+      add_help_to_end(message)
+    when '/info'
+      info(message)
+      add_help_to_end(message)
+    when '/filter'
+      clear_filter
+      ask_filter_chronicle(message)
+    when '/notify'
+      @bot.api.send_message(chat_id: message.chat.id, text: "Not implemented yet!")
+    when 'C1-C4'
+      @user.filter_chronicle = @user.filter_chronicle + ['c1', 'c2', 'c3', 'c4']
+      send_chronicle_filter(message)
+      ask_filter_chronicle(message)
+    when 'Interlude'
+      @user.filter_chronicle = @user.filter_chronicle + ['interlude', 'interlude+']
+      send_chronicle_filter(message)
+      ask_filter_chronicle(message)
+    when 'High five'
+      @user.filter_chronicle = @user.filter_chronicle + ['high five']
+      send_chronicle_filter(message)
+      ask_filter_chronicle(message)
+    when 'Epilogue'
+      @user.filter_chronicle = @user.filter_chronicle + ['epilogue']
+      send_chronicle_filter(message)
+      ask_filter_chronicle(message)
+    when 'Classic'
+      @user.filter_chronicle = @user.filter_chronicle + ['classic']
+      send_chronicle_filter(message)
+      ask_filter_chronicle(message)
+    when I18n.t('.other')
+      @user.filter_chronicle = @user.filter_chronicle + ['final', 'lindvior', 'freya', 'ertheria', 'odyssey']
+      send_chronicle_filter(message)
+      ask_filter_chronicle(message)
+    when I18n.t('enough')
+      ask_filter_rates(message)
+    when '0-15'
+      @user.filter_rates_from = 0
+      @user.filter_rates_to = 15
+      ask_for_notifies(message)
+    when '15-100'
+      @user.filter_rates_from = 15
+      ask_for_notifies(message)
+      @user.filter_rates_to = 100
+    when '100-250'
+      @user.filter_rates_from = 100
+      @user.filter_rates_to = 250
+      ask_for_notifies(message)
+    when '250+'
+      @user.filter_rates_from = 250
+      @user.filter_rates_to = 99999999
+      ask_for_notifies(message)
+    when 'RU'
+      @user.lang = 'ru'
+      I18n.locale = :ru
+      add_help_to_end(message)
+    when 'ENG'
+      @user.lang = 'en'
+      I18n.locale = :en
+      add_help_to_end(message)
+      when I18n.t('dont_notify')
+        @user.last_notified = Time.now.iso8601
+        filter(message)
+        @user.notify_period = nil
+      when I18n.t('notify_everyday')
+        @user.last_notified = Time.now.iso8601
+        filter(message)
+        @user.notify_period = 1
+      when I18n.t('notify_everyweek')
+        @user.last_notified = Time.now.iso8601
+        filter(message)
+        @user.notify_period = 7
+      when I18n.t('notify_everymonth')
+        @user.last_notified = Time.now.iso8601
+        filter(message)
+        @user.notify_period = 30
+    else
+      @bot.api.send_message(chat_id: message.chat.id, text: message.text)
     end
+  end
 
-    def send_servers(servers, message)
-      servers.each do |s|
-        day_diff = date_diff(Time.now, s[3])
-        day_part =  if (day_diff > 0) then
-                      I18n.t('opens_in_x_days', x: day_diff)
-                    elsif (day_diff < 0) then
-                      I18n.t('opened_x_days_ago', x: -day_diff)
-                    else
-                      I18n.t('opens_today')
-                    end
-        text = "#{s[0]}\n#{s[1]} x#{s[2]}\n#{s[3]} #{day_part}"
-        @bot.api.send_message(chat_id: message.chat.id, text: text)
+  def respond_to_callback_query(message)
+    # case message.data
+    # end
+  end
+
+  def send_servers(servers, message)
+    servers.each do |s|
+      day_diff = date_diff(Time.now, s[3])
+      day_part =  if (day_diff > 0) then
+                    I18n.t('opens_in_x_days', x: day_diff)
+                  elsif (day_diff < 0) then
+                    I18n.t('opened_x_days_ago', x: -day_diff)
+                  else
+                    I18n.t('opens_today')
+                  end
+      text = "#{s[0]}\n#{s[1]} x#{s[2]}\n#{s[3]} #{day_part}"
+      @bot.api.send_message(chat_id: message.chat.id, text: text)
+    end
+  end
+
+  def ask_lang(message)
+    kb = [
+      [Telegram::Bot::Types::KeyboardButton.new(text: 'RU'),
+      Telegram::Bot::Types::KeyboardButton.new(text: 'ENG')]
+    ]
+    markup = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: kb, one_time_keyboard: true)
+    @bot.api.send_message(chat_id: message.chat.id, text: "LANGUAGE", reply_markup: markup)
+  end
+
+  def ask_filter_chronicle(message)
+    markup = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: filter_keyboard, one_time_keyboard: true)
+    @bot.api.send_message(chat_id: message.chat.id, text: I18n.t('select_chronicle'), reply_markup: markup)
+  end
+
+  def ask_filter_rates(message)
+    kb = [
+      Telegram::Bot::Types::KeyboardButton.new(text: '0-15'),
+      Telegram::Bot::Types::KeyboardButton.new(text: '15-100'),
+      Telegram::Bot::Types::KeyboardButton.new(text: '100-250'),
+      Telegram::Bot::Types::KeyboardButton.new(text: '250+'),
+    ]
+    markup = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: kb, one_time_keyboard: true)
+    @bot.api.send_message(chat_id: message.chat.id, text: I18n.t('choose_rates'), reply_markup: markup)
+  end
+
+  def ask_for_notifies(message)
+    kb = [
+      Telegram::Bot::Types::KeyboardButton.new(text: I18n.t('dont_notify')),
+      Telegram::Bot::Types::KeyboardButton.new(text: I18n.t('notify_everyday')),
+      Telegram::Bot::Types::KeyboardButton.new(text: I18n.t('notify_everyweek')),
+      Telegram::Bot::Types::KeyboardButton.new(text: I18n.t('notify_everymonth')),
+    ]
+    markup = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: kb, one_time_keyboard: true)
+    @bot.api.send_message(chat_id: message.chat.id, text: I18n.t('should_we_notify'), reply_markup: markup)
+  end
+
+  def send_chronicle_filter(message)
+    @bot.api.send_message(chat_id: message.chat.id, text: I18n.t('selected_chronicles', x: @user.filter_chronicle.join(", ")))
+  end
+
+  def help(message)
+    @bot.api.send_message(chat_id: message.chat.id, text: "#{I18n.t('command_list')}\n/soon\n/recent\n/filter\n/info\n/help")
+  end
+
+  def info(message)
+    @bot.api.send_message(chat_id: message.chat.id, text: "#{I18n.t('total_servers')} #{@all.length}")
+  end
+
+  def add_help_to_end(message)
+    @bot.api.send_message(chat_id: message.chat.id, text: I18n.t('need_help'))
+  end
+
+  def soon(servers, days)
+    servers.select do |server|
+      date_diff(Time.now, server[3]) > 0 && date_diff(Time.now, server[3]) <= days
+    end.sort do |s1, s2|
+      date_diff(Time.now, s2[3]) <=> date_diff(Time.now, s1[3])
+    end
+  end
+
+  def recent(servers, days)
+    servers.select do |server|
+      date_diff(Time.now, server[3]) < 0 && date_diff(Time.now, server[3]) >= -days
+    end.sort do |s1, s2|
+      date_diff(Time.now, s1[3]) <=> date_diff(Time.now, s2[3])
+    end
+  end
+
+  def from_to(servers, from, to)
+    servers.select do |server|
+      date_diff(Time.now, server[3]) >= from && date_diff(Time.now, server[3]) <= to
+    end.sort do |s1, s2|
+      date_diff(Time.now, s2[3]) <=> date_diff(Time.now, s1[3])
+    end
+  end
+
+  def apply_chonicle_filter(servers, chronicles)
+    servers.select do |server|
+      chronicles.include? server[1]
+    end
+  end
+
+  def apply_rates_filter(servers, from, to)
+    servers.select do |server|
+      server[2].to_i >= from.to_i && server[2].to_i <= to.to_i
+    end
+  end
+
+  def clear_filter
+    @user.filter_chronicle = []
+  end
+
+  def filter(message)
+    send_servers(
+      from_to(
+        apply_chonicle_filter(
+          apply_rates_filter(
+            @all,
+            @user.filter_rates_from,
+            @user.filter_rates_to
+          ),
+          @user.filter_chronicle
+        ),
+        -14,
+        14
+      ),
+      message
+    )
+    add_help_to_end(message)
+  end
+
+  def date_diff(*dates)
+    days = 0
+    dates.each_with_index do |date, i|
+      if date.is_a? Time then
+        d = date.day + date.month*30 + date.year*365
+      else
+        parsed = date.split('.').map! { |x| x.to_i }
+        d = parsed[0] + parsed[1]*30 + (parsed[2]+2000)*365
       end
+      days += (i.even? ? -d : d)
     end
-
-    def help(message)
-      @bot.api.send_message(chat_id: message.chat.id, text: "#{I18n.t('command_list')}\n/soon\n/recent\n/info\n/help")
-    end
-
-    def info(message)
-      @bot.api.send_message(chat_id: message.chat.id, text: "#{I18n.t('total_servers')} #{@all.length}")
-    end
-
-    def soon(servers, days)
-      servers.select do |server|
-        date_diff(Time.now, server[3]) > 0 && date_diff(Time.now, server[3]) <= days
-      end.sort do |s1, s2|
-        date_diff(Time.now, s2[3]) <=> date_diff(Time.now, s1[3])
-      end
-    end
-
-    def add_help_to_end(message)
-      @bot.api.send_message(chat_id: message.chat.id, text: t('need_help'))
-    end
-
-    def recent(servers, days)
-      servers.select do |server|
-        date_diff(Time.now, server[3]) < 0 && date_diff(Time.now, server[3]) >= -days
-      end.sort do |s1, s2|
-        date_diff(Time.now, s1[3]) <=> date_diff(Time.now, s2[3])
-      end
-    end
-
-    def filter(servers, chronicle, rates_from, rates_to)
-
-    end
-
-    def date_diff(*dates)
-      days = 0
-      dates.each_with_index do |date, i|
-        if date.is_a? Time then
-          d = date.day + date.month*30 + date.year*365
-        else
-          parsed = date.split('.').map! { |x| x.to_i }
-          d = parsed[0] + parsed[1]*30 + (parsed[2]+2000)*365
-        end
-        days += (i.even? ? -d : d)
-      end
-      days
-    end
+    days
+  end
 end
--- models/user.rb
+++ models/user.rb
@@ -1,14 +1,64 @@
 class User
-  attr_reader :user_id, :filter_from, :filter_to, :filter_chronicle, :filter_rates_from, :filter_rates_to
+  attr_reader :user_id, :filter_from, :filter_to, :filter_rates_from, :filter_rates_to, :lang, :last_notified, :notify_period
 
   def initialize(db, id)
-    values    = db.exec("SELECT * from users WHERE user_id = $1", [id.to_s]).values
+    @db       = db
+    values    = @db.exec("SELECT * from users WHERE user_id = $1", [id.to_s]).values[0]
 
-    @user_id            = values[0]
-    @filter_from        = values[1]
-    @filter_to          = values[2]
-    @filter_chronicle   = values[3]
-    @filter_rates_from  = values[4]
-    @filter_rates_to    = values[5]
+    @user_id            = values[0].to_s
+    @filter_from        = values[1].to_s
+    @filter_to          = values[2].to_s
+    @filter_chronicle   = values[3].to_s
+    @filter_rates_from  = values[4].to_s
+    @filter_rates_to    = values[5].to_s
+    @lang               = values[6].to_s
+    @last_notified      = values[7].to_s
+    @notify_period      = values[8].to_i
+    puts values
+  end
+
+  def filter_from=(val)
+    @db.exec "UPDATE users SET filter_from = $1 WHERE user_id = $2;", [val.to_s, @user_id.to_s]
+    @filter_from = val
+  end
+
+  def filter_to=(val)
+    @db.exec "UPDATE users SET filter_to = $1 WHERE user_id = $2;", [val.to_s, @user_id.to_s]
+    @filter_to = val
+  end
+
+  def filter_chronicle
+    @filter_chronicle.empty? ? [] : JSON.parse(@filter_chronicle)
+  end
+
+  def filter_chronicle=(val)
+    val = val.uniq.to_json
+    @db.exec "UPDATE users SET filter_chronicle = $1 WHERE user_id = $2;", [val.to_s, @user_id.to_s]
+    @filter_chronicle = val
+  end
+
+  def filter_rates_from=(val)
+    @db.exec "UPDATE users SET filter_rates_from = $1 WHERE user_id = $2;", [val.to_s, @user_id.to_s]
+    @filter_rates_from = val
+  end
+
+  def filter_rates_to=(val)
+    @db.exec "UPDATE users SET filter_rates_to = $1 WHERE user_id = $2;", [val.to_s, @user_id.to_s]
+    @filter_rates_to = val
+  end
+
+  def lang=(val)
+    @db.exec "UPDATE users SET lang = $1 WHERE user_id = $2;", [val.to_s, @user_id.to_s]
+    @lang = val
+  end
+
+  def last_notified=(val)
+    @db.exec "UPDATE users SET last_notified = $1 WHERE user_id = $2;", [val.to_s, @user_id.to_s]
+    @last_notified = val
+  end
+
+  def notify_period=(val)
+    @db.exec "UPDATE users SET notify_period = $1 WHERE user_id = $2;", [val.to_i, @user_id.to_s]
+    @notify_period = val.to_i
   end
 end
