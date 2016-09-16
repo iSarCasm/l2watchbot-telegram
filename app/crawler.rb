1d010e4b1538d93cb89af837febe3081e660ef24
SarCasm <sarcasm008@gmail.com>
2016-09-17 00:16:17 +0300

    mails

--- app/crawler.rb
+++ app/crawler.rb
@@ -68,7 +68,7 @@ class Crawler
   def send_warn_email(server_count)
     return if server_count > 250     # meh
     mail = Mail.new do
-      # from    'crawler@l2watch.bot'
+      from    'root@l2watch.bot'
       to      'sarcasm008@gmail.com'
       subject 'L2Watchbot Crawler warning!'
       body    "While scanning #{SOURCE_WEBSITE} at #{Time.now.to_s} found only #{server_count} servers."
